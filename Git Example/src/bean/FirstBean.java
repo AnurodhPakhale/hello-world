fb6a73a2bc531eebcfc6f16db0065d2e578a86d5
anurodhpakhale777 <37894662+anurodhpakhale777@users.noreply.github.com>
2019-09-08 01:26:16 +0530

    todo deleted

--- Git Example/src/bean/FirstBean.java
+++ Git Example/src/bean/FirstBean.java
@@ -4,8 +4,7 @@ package bean;
 public class FirstBean {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		System.out.println("Git Project");
 		System.out.println("Line added in github site");
 	}
